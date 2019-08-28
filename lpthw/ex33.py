d3d0f523c74f9e839e50aff76a6d18d1a4fa555e
echocruise <xiask2018@mt.nai.edu.cn>
2019-08-28 10:54:35 +0800

    ex34 ex35

--- lpthw/ex33.py
+++ lpthw/ex33.py
@@ -1,12 +1,7 @@
-def fun(n):
-  i = 0
-  numbers = []
-  while i < n:
-    print(f"At the top is {i}")
-    i +=1
-    print("Number now:", numbers)
-  print("The numbers:")
-  for num in numbers:
-      print(num)
 nu = int(input("Pleas input a number:"))
-fun(nu)
+inc = int(input("Pleas set the increments:"))
+def fun(nu,inc):
+  for i in range(0,nu,inc):
+    print(i)
+
+fun(nu,inc)
